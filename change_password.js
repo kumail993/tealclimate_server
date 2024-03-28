cf9b2d13761fa0ce1cee256b3c03196e6fdce36c
kumail <khaider308@gmail.com>
2024-03-28 16:29:22 +0500

    testing

--- change_password.js
+++ change_password.js
@@ -6,7 +6,7 @@ const db = require('./db.js');
 router.post('/', async (req, res) => {
   try {
     const { loginId, oldPassword, newPassword } = req.body;
-    print(loginId, oldPassword, newPassword);
+    console.log(loginId, oldPassword, newPassword);
 
     // Check if loginId, oldPassword, and newPassword are provided
     if (!loginId || !oldPassword || !newPassword) {

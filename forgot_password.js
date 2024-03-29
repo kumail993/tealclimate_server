6e862341abc80c2ca4796280f5d2e59cabe01666
kumail <khaider308@gmail.com>
2024-03-29 16:59:25 +0500

    testing

--- forgot_password.js
+++ forgot_password.js
@@ -30,14 +30,71 @@ router.post('/', async (req, res) => {
     });
 
     const mailOptions = {
-      from: 'Khaider308@gmail.com',
-      to: email, // Recipient's email
-      subject: 'Your Teal-Climate Verification Code',
-      html: `<html>
-        <!-- Your HTML content here -->
-        <div class="otp">${otp}</div>
-        <!-- More HTML content -->
-      </html>`
+        from: "Khaider308@gmail.com",
+        to: email, // Recipient's email
+        subject: "Your Teal-Climate Verification Code",
+        html: `<html>
+        <head>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            background-color: white;
+            margin: 0;
+            padding: 0;
+          }
+          .container {
+            max-width: 600px;
+            margin: 0 auto;
+            padding: 20px;
+            border: 1px solid #ddd;
+          }
+          .header {
+            color: #008080; /* Teal */
+            font-size: 24px;
+            margin-bottom: 10px;
+            text-align: center;
+          }
+          .info {
+            font-size: 18px;
+            margin-bottom: 20px;
+            text-align: center;
+            color: #333; /* Dark Gray */
+          }
+          .otp {
+            font-weight: bold;
+            font-size: 28px;
+            color: #008080; /* Teal */
+            text-align: center;
+            margin: 20px 0;
+          }
+          .footer {
+            font-size: 14px;
+            margin-top: 20px;
+            color: white;
+            background-color: #008080; /* Teal */
+            padding: 10px;
+            text-align: center;
+          }
+        </style>
+      </head>
+      <body>
+        <div class="container">
+          <div class="header">Verification Email</div>
+          <div class="info">
+            Hello, ${email}!<br>
+            We're delighted to welcome you to our TealClimate community.
+          </div>
+          <div class="otp">${otp}</div>
+          <div class="info">
+            Use the verification code above to complete your registration process. If you haven't initiated this request, kindly disregard this email.
+          </div>
+          <div class="footer">
+            Best regards,<br>
+            The Teal-Climate Team
+          </div>
+        </div>
+      </body>
+        </html>`
     };
 
     const info = await transporter.sendMail(mailOptions);
